0e2c44f38bc148ecf216b423d0ec47e81326ec57
guitfidbob <chavez.bobby@gmail.com>
2019-11-07 19:52:47 -0500

    Initial file structure for FanCentral MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FanCentral.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
